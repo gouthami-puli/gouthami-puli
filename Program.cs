a5744f637615116da93f2590aef75c66461f7ff6
Josef Karim <josefkarim@live.se>
2022-09-21 13:57:50 +0200

    hi again

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace testgithub
         static void Main(string[] args)
         {
             Console.WriteLine("Hi from Josef!");
+
+            Console.WriteLine("Hi again!!");
         }
     }
 }
